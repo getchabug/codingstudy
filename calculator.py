b681dae90b056b0cb4dd595cfbd4ef4aad12ff5f
sg070309 <sk070309@naver.com>
2018-12-23 21:25:56 +0900

    나누기 기능 추가 (승건)

--- calculator.py
+++ calculator.py
@@ -4,9 +4,14 @@ class Calculator:
 	def adder(self,num):
 		self.result+=num
 		return self.result
+#lucas
+	def divide(self,num):
+		self.result/=num
+		return self.result 
 cal1=Calculator()
 cal2=Calculator()
 print(cal1.adder(3))
 print(cal1.adder(4)) 
 print(cal2.adder(3))
 print(cal2.adder(7))
+print(cal2.divide(5))
