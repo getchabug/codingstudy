3312f3509518e18da3678e9058368fc4cc4b07c5
parkjaehyun <parkjaehyun.070929@gmail.com>
2018-12-23 21:27:31 +0900

    제곱 기능 추가 ( 재현)

--- calculator.py
+++ calculator.py
@@ -8,6 +8,10 @@ class Calculator:
 	def divide(self,num):
 		self.result/=num
 		return self.result 
+
+	def jaegop(self,num):
+		self.result**=num
+		return self.result	
 cal1=Calculator()
 cal2=Calculator()
 print(cal1.adder(3))
@@ -15,3 +19,5 @@ print(cal1.adder(4))
 print(cal2.adder(3))
 print(cal2.adder(7))
 print(cal2.divide(5))
+print(cal2.jaegop(3))
+print(cal2.jaegop(7))
