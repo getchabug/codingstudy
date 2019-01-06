dfbc7953f765a03e7f6724e18ab902c917927a6d
lmania0126 <lmania0126@gmail.com>
2019-01-06 21:11:56 +0900

    Create FourCal.py

---
+++ 성주/FourCal.py
@@ -0,0 +1,16 @@
+class FourCal:
+	def setdata(self,first,second):
+		self.first = first
+		self.second = second
+	def sum(self):
+		result = self.first + self.second
+		return result
+	def mul(self):
+		result = self.first * self.second
+		return result
+	def sub(eelf):
+		result = self.first - self.second
+		return result
+	def div(self):
+		result = self.first / self.second
+		return result
