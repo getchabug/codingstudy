6217ebd9857e1b37d4b0b5f73ae2ae397d7919c8
3ruqtkf <45330384+3ruqtkf@users.noreply.github.com>
2019-01-20 20:19:53 +0900

    ss
    ss

---
+++ 준혁/JH.py
@@ -0,0 +1,8 @@
+class codingstudy:
+	lastname=coding
+	def __init_(self,name):
+		self.fullname=self.lastname+name
+	def __init__(self,study):
+		print("%s,%s공부를 하다."%(self,fullname,study))
+	def __init__(self,sleep):
+		print("%s,%s잠 자고 있다."%(self,fullname,sleep))
