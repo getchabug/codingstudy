ece837ee2a2478522a3e19eecb42baa525701b17
symnoisy <symnoisy@gmail.com>
2019-05-12 14:24:30 +0900

    Add Algorithm Format

---
+++ Teacher/main.py
@@ -0,0 +1,6 @@
+from test_module import Algorithm
+
+test_module = Algorithm()
+
+plus_result = test_module.plus(1, 2)
+print(plus_result)
---
+++ Teacher/test_module.py
@@ -0,0 +1,7 @@
+class Algorithm:
+    def __init__(self):
+        self.result = 0
+
+    def plus(self, a, b):
+        self.result = a + b
+        return self.result
