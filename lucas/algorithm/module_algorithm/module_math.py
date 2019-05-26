dff9bf33923b0cb502d7bd9cc831fa7c6d99fc28
sg070309 <sk070309@naver.com>
2019-05-26 21:24:03 +0900

    수업 끝

--- lucas/algorithm/module_algorithm/module_math.py
+++ lucas/algorithm/module_algorithm/module_math.py
@@ -1,1 +1,12 @@
-#aa
+
+class Math:
+    def __init__(self):
+        self.result=0
+
+    def sum_n(self,param1):
+
+        for sum_target in range(1,param1+1):
+            self.result=self.result+sum_target
+        return self.result
+    def mul_n(self,param1):
+      print("hello")
