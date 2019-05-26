6a3ac423a9665048c776faa1de533cd61120dc9d
julianpark0812 <julianpark0812@naver.com>
2019-05-26 21:23:45 +0900

    수업완료

---
+++ julian/algorithm/test_algorithm.py
@@ -0,0 +1,9 @@
+#-*- coding: utf-8 -*-
+#module_algorthim 디렉토리에서 만든 module_math.py의
+#Math 클래스를 호출
+from module_math import Math
+
+#Math 클래스에서 호출해서 cal
+calculate = Math()
+
+print(calculate.sum_n(100))
