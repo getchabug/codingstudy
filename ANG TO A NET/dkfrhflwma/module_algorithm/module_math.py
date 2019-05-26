eab072525ae144fa9f800036d02a9c9abaaa11fd
3ruqtkf <45330384+3ruqtkf@users.noreply.github.com>
2019-05-26 19:50:34 +0900

    숙제 완성작(준혘)

---
+++ ANG TO A NET/dkfrhflwma/module_algorithm/module_math.py
@@ -0,0 +1,26 @@
+#-*- coding: utf-8 -*-
+#수학 클래스 정의
+class Math:
+    #이 함수는 클래스 내에서 사용하는 변수 정의
+    def __init__(self):
+        self.result=0
+    #param1,param2를 입력값으로 받아서, 더하기 연산을하고 self.result에 저장 한다음 return
+    def add_2param(self,param1,param2):
+        self.result=param1+param2
+
+        return self.result
+    #param1,param2를 입력값으로 받아서, 곱하기 연산을 하고 self.result에 저장 한다음 return
+    def mul_2param(self,param1,param2):
+        self.result=param1*param2
+
+        return self.result
+    #pram1,param2를 입력삾으로 받아서,나누기 연산을 하고 self.result에 저장 한다음 return
+    def sub_2param(self,param1,param2):
+        self.result=param1%param2
+
+        return self.result
+    #param1,param2를 입력값으로 받아서,빼기 연산을 하고 self.result에 저장 한다음 return
+    def min_2param(self,param1,param2):
+        self.result=param1-param2
+
+        return self.resut
