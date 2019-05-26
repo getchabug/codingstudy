969e4f777d24a49cc233ff6ee52c02bdaad39ce1
symnoisy <symnoisy@gmail.com>
2019-05-26 19:56:52 +0900

    불필요한 파일 삭제

--- 천재_승건/8주차숙제_승건.py
+++
@@ -1,8 +0,0 @@
-def sum(a, b):
-	return a + b
-
-def mum(a,b):
-	return a * b
-
-def pius(a,b,c):
-	return a + b *c
--- 천재_승건/class.py
+++
@@ -1,9 +0,0 @@
-class play:
-		
-	result=9
-	def __init__(self, num):
-		self.num = 9087654
-
-	def game(self, num):
-		self.result +=num
-		return self.result
