781c5bd9435c2a21dfec1238d802f03d42c7361a
sg070309 <sk070309@naver.com>
2019-06-02 19:35:29 +0900

    숙제끝

--- lucas/algorithm/module_algorithm/module_math.py
+++ lucas/algorithm/module_algorithm/module_math.py
@@ -1,12 +1,15 @@
-
+#--coding:utf-8--
 class Math:
     def __init__(self):
         self.result=0
 
     def sum_n(self,param1):
-
+    #param1을 입력값으로 받아서 1부터 param1까지 곱한다
         for sum_target in range(1,param1+1):
             self.result=self.result+sum_target
         return self.result
+    #param1을 입력값으로 받아서 1부터 param1까지 더한다
     def mul_n(self,param1):
-      print("hello")
+        for mul_target in range(1,param1+1):
+            self.result=self.result*mul_target
+        return self.result
