05e45b6282626edf5435e49ec68b674b93f89826
sg070309 <sk070309@naver.com>
2019-06-09 17:18:19 +0900

    숙제끝

--- lucas/algorithm/test_algorithm.py
+++ lucas/algorithm/test_algorithm.py
@@ -1,4 +1,18 @@
-
-from module_math import Math
-cal=Math()
-print(cal.sum_n(100))
+class Calculator:
+    def __init__(self, input_list):
+        self.sum=0
+        self.list= input_list
+    def sum_(self):
+        for i in self.list:
+             self.sum += i
+        return self.sum
+    def avg(self):
+        result_sum = self.sum_()
+        a = result_sum/len(self.list)
+        return a
+cal1=Calculator([1,2,3,4,5])
+print(cal1.sum_())
+print(cal1.avg())
+cal2=Calculator([6,7,8,9,10])
+print(cal2.sum_())
+print(cal2.avg())
