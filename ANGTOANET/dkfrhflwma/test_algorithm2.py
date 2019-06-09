4000da77ab11d66dec1175d3bb2a761b26624d37
3ruqtkf <45330384+3ruqtkf@users.noreply.github.com>
2019-06-09 17:15:53 +0900

    숙제

--- ANGTOANET/dkfrhflwma/test_algorithm2.py
+++ ANGTOANET/dkfrhflwma/test_algorithm2.py
@@ -1,4 +1,8 @@
 from module_math import Math
 test_class=Math()
-a=[17, 92, 18, 33, 58, 7, 33, 42]
+a=[17, 92, 18, 33, 58, 7,33,42]
 print(test_class.biggest(a))
+
+from module_math import Math
+
+test_class=Math()(test_class.smallest(a))
