9342e7e0514a64bfddb5c0b884dd69dd5db3236b
3ruqtkf <45330384+3ruqtkf@users.noreply.github.com>
2019-06-23 15:21:17 +0900

    숙제

--- ANGTOANET/dkfrhflwma/module_algorithm/module_math.py
+++ ANGTOANET/dkfrhflwma/module_algorithm/module_math.py
@@ -1,5 +1,6 @@
 #-*- coding: utf-8 -*-
 #수학 클래스 정의
+from collections import Counter
 class Math:
     #이 함수는 클래스 내에서 사용하는 변수 정의
     def __init__(self):
@@ -34,6 +35,7 @@ class Math:
 
 
 
+
 #param1,param2를 입력값으로 받아서,더하기 연산을 하고 self.result에 저장 한다음 return
     def sum_n(self,param1):
         for sum_target in range(1,param1+1):
@@ -48,4 +50,19 @@ class Math:
         for mul_target in range(1,param1-1):
             self.reslt=self.result
 
+     def samename(self,list1):
+        test_dict=dict(Counter['Tom','Jerry','Mike','Tom'])
+        keys=list(test_dict.keys())
+        value=list(test_dict.value())
+        for value_idx,value_count in enumerate(values):
+            if value_count>
+
+
+     def same(self, list4):
+         test_dict=dict(Counter(['Tom','Mike','Jerry','Tom']))
+         keys=list(test_dict.keys())
+        value=list(test_dict.values())
+        for value_idx, value_count in enumerate(values):
+            if value_count <2:
+                return keys[value_idx]
 
