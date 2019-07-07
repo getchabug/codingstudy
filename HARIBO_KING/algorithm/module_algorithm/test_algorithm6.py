4052baeadcd8d6b84d71edb9fa1a5bbcb71c0f84
lmania0126 <lmania0126@gmail.com>
2019-07-07 16:26:31 +0900

    숙제했어요리사

---
+++ HARIBO_KING/algorithm/module_algorithm/test_algorithm6.py
@@ -0,0 +1,12 @@
+def lcm(a,b):
+   if a > b:
+       i = a
+   else:
+       i = b
+   while(True):
+       if((i % a == 0) and (i % b == 0)):
+           lcm = i
+           break
+       i += 1
+   return lcm
+print(lcm(4,16))
