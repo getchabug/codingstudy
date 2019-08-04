aad9161ea2669ae8fb76b7d17c2ae368badcdf87
sg070309 <sk070309@naver.com>
2019-08-04 18:07:19 +0900

    숙제 끝

--- lucas/algorithm/Class_Make.py
+++ lucas/algorithm/Class_Make.py
@@ -1,42 +1,54 @@
 
 
-class test:
+
+class game:
     def __init__(self):
-        self.math=0
-        self.googu=0
-        self.science=0
-        self.society=0
-    def seahum(self,math,googu,science,society,):
-        self.math=math
-        self.googu=googu
-        self.science=science
-        self.society=society
-        total=math+googu+science+society
-        if total >200:
-            s=("시험에 합격하셨습니다")
-            return s
-        elif total < 200:
-            p=("시험에 낙제하였습니다")
-            return p
+        self.power=0
+        self.defence=0
+
+    def baseball(self,power,defence):
+        self.power=power
+        self.defence=defence
+        if power > defence:
+            r=("홈런입니다")
+            return r
+        elif power < defence:
+            r=("스트라이크!!")
+            return r
+        elif power == defence:
+            r=("무승부!!")
+            return r
+fey=game()
+t=fey.baseball(4,5)
+print(t)
 
 
-result=test()
-i=result.seahum(1,1,1,1)
-print(i)
 
-class aircorn:
-    def __init__(self):
-        self.cool=0
-        self.cool_level=0
-    def eurcorn(self,cool,cool_level):
-        self.cool=cool
-        self.cool_level=cool_level
-        total=cool+cool_level
-        return total
 
-fey=aircorn()
-g=fey.eurcorn(3,6)
-print(g)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
