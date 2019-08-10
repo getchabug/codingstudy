f58cf4667e2cfc0a1172db87411a9ee69a409dc0
parkjaehyun <parkjaehyun.070929@gmail.com>
2019-08-10 13:13:01 +0900

    숙제

--- michael/algorithm/test_algorithm4.py
+++ michael/algorithm/test_algorithm4.py
@@ -1,31 +1,8 @@
-a=[3,2,1]
-b=[]
-c=[]
-count = 0
-
-def blockchecker(list_a,list_b,count):
-    list_b.append(list_a.pop())
-    count=count+1
-
-blockchecker(a,c,count)
-blockchecker(a,b,count)
-blockchecker(a,c,count)
-a.pop()
-b.insert(0,2)
-count=(count+1)
-c.pop()
-b.insert(1,1)
-count=(count+1)
-a.pop()
-c.insert(0,3)
-count=(count+1)
-b.pop()
-a.insert(0,1)
-count=(count+1)
-b.pop()
-c.insert(1,2)
-count=(count+1)
-a.pop()
-c.insert(2,1)
-count=(count+1)
-print(count)
+class coding:
+    def __init__(self):
+        self.result=0
+    def sum(self,a,b,c):
+        self.result=a*b/c
+        return self.result
+coding=coding()
+print(coding.sum(5,9,3))
