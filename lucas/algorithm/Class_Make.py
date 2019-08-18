d5df5559812862d99a703966fd1dbbcab6af3e87
sg070309 <sk070309@naver.com>
2019-08-18 19:03:50 +0900

    숙제 끝

--- lucas/algorithm/Class_Make.py
+++ lucas/algorithm/Class_Make.py
@@ -1,24 +1,22 @@
 
-class tenten:
+
+class shpoping:
     def __init__(self):
-        self.tentan=0
-        self.height=0
-    def tanten(self,tentan,height):
-        self.tentan=tentan
-        self.height=height
-        tall=tentan*3
-        total=height+tall
-        return total
-
-fey=tenten()
-a=fey.tanten(5,140)
+        self.have_money=0
+        self.price=0
+    def shop(self,have_money,price):
+        self.have_money=have_money
+        self.price=price
+        total=have_money-price
+        if price <= have_money:
+            r=("감사합니다 호갱님,거스름돈은 %s입니다"%total)
+            return r
+        else:
+            r=("돈도 없는게어디서 절로꺼져!!")
+            return r
+fey=shpoping()
+a=fey.shop(7000,600)
 print(a)
-
-
-
-
-
-
 
 
 
