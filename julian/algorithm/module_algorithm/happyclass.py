78fb45511660455a7aa1c3bb25d9f5aa24dd0be6
julianpark0812 <julianpark0812@naver.com>
2019-09-15 19:03:02 +0900

    숙제 끝

--- julian/algorithm/module_algorithm/happyclass.py
+++ julian/algorithm/module_algorithm/happyclass.py
@@ -1,15 +1,11 @@
-class Math:
+class ddong:
     def __init__(self):
         self.result = 0
 
 
+    def smallest(self, list1):
+        list1.sort()
+        return list1.pop(0)
 
-
-def mul_n(self, a):
-    print("hello")
-    for sum_target in range(1, a * 1):  # 그래서 1부터 param1을 곱한더
-        self.result = self.result * sum_target
-    return self.result
-
-cal=Math()
-print(cal.mul_n(3))
+cal = ddong()
+print(cal.smallest(2))
