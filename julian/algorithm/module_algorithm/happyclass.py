43450f7c2c058e188495596ac96556436fb12092
julianpark0812 <julianpark0812@naver.com>
2019-09-22 18:46:36 +0900

    숙제 끝

--- julian/algorithm/module_algorithm/happyclass.py
+++ julian/algorithm/module_algorithm/happyclass.py
@@ -1,11 +1,12 @@
-class ddong:
+class d:
+
     def __init__(self):
         self.result = 0
 
 
-    def smallest(self, list1):
+    def biggest(self, list1):
         list1.sort()
-        return list1.pop(0)
+        return list1.pop()
 
-cal = ddong()
-print(cal.smallest(2))
+cal = d()
+print(cal.biggest(3))
